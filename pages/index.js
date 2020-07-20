f207c6d81faa398c3b7de5230c00c7a52f4301e0
Dom Steil <dominic@dapps-inc.com>
2020-07-20 15:49:04 -0700

    adding next

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
 import { CID } from 'ipfs-http-client'
+import useIpfs from '../components/hooks/use-ipfs'
+
+export async function getStaticProps() {
+    const CID = await useIpfs();
+  
+    return { props: { CID } };
+  }
+
+export default function Index({ CID }) {
+    return (
+
+        <>
+        <h1>stateset-fs demo</h1>
+        <h2>distributed hash table lookup for content addressed documents that are then shown as static generated html.</h2>
+        </>
+    );
+}
